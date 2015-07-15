442bfc93118b870519abd2bbf4ff28b4e802c41b
shihong <shihong.zt@kaicong.net>
2015-07-15 11:33:19 +0800

    tijiao

---
+++ Testdd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
